3d784d564bc72e5ac364555e6163be26b776eba1
AndreaFronzi <andrifronzi@gmail.com>
2025-07-16 18:13:13 +0200

    Add Angular Material and CDK, update global styles
    - Added `@angular/material` and `@angular/cdk` dependencies to `package.json`.
    - Updated styles with Roboto font and Material default typography.
    - Removed placeholder content and cleaned up `app.component.html`.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component,ChangeDetectionStrategy } from '@angular/core';
+import {ProvaComponent} from './prova/prova.component';
+import {ButtonProvaComponent} from './button-prova/button-prova.component';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    ProvaComponent, ButtonProvaComponent,MatProgressBarModule,MatChipsModule,
+    MatCardModule,MatIconModule,MatDividerModule,MatButtonModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'corso-angular';
 }
+
